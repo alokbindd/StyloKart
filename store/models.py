22a900008015ce84c7dbb6c5cb74f8f38684a751
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-01-22 12:03:56 +0530

    created new app store and models for it

---
+++ store/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from category.models import category
+
+# Create your models here.
+
+class Product(models.Model):
+    product_name    = models.CharField(max_length=200,unique=True)
+    slug            = models.SlugField(max_length=100)
+    description     = models.TextField(max_length=250)
+    product_images  = models.ImageField(upload_to='photos/product')
+    price           = models.IntegerField()
+    stock           = models.IntegerField()
+    is_available    = models.BooleanField(default=True)
+    category        = models.ForeignKey(category, on_delete=models.CASCADE)
+    created_at      = models.DateTimeField(auto_now_add=True)
+    modified_at     = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.Product_name
