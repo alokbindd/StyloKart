7f9ca7a98b3076456c259510b061d374c66464b9
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-01-23 14:26:32 +0530

    created a geturl function using reverse to get urls for single product

--- store/models.py
+++ store/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from category.models import category
-
+from django.urls import reverse
 # Create your models here.
 
 class Product(models.Model):
@@ -14,6 +14,9 @@ class Product(models.Model):
     category        = models.ForeignKey(category, on_delete=models.CASCADE)
     created_at      = models.DateTimeField(auto_now_add=True)
     modified_at     = models.DateTimeField(auto_now=True)
+
+    def get_url(self):
+        return reverse('product_detail', kwargs={'category_slug':self.category.slug,'product_slug':self.slug})
 
     def __str__(self):
         return self.product_name
