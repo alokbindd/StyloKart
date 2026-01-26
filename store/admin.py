cd80cb24ea9b61c06f30a3de9cfb9b6e8658b647
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-01-26 15:30:21 +0530

    registered Variation model and VariationAdmin.

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product ,Variation
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name','price', 'stock', 'category', 'modified_at', 'is_available')
     prepopulated_fields = {'slug':('product_name', )}
 
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product','variation_category','variation_value','is_active',)
+    list_filter = ('product','variation_category','variation_value',)
+    list_editable = ('is_active',)
+    
+
 # Register your models here.
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
