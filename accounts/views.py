8501f35232bfae22c0e5b9e479a77f68dc927405
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-01-30 19:37:17 +0530

    Preserve checkout redirect for guest users after login

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,9 @@ from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages ,auth
 from django.contrib.auth.decorators import login_required
-
+from carts.models import Cart, CartItem
+from carts.views import _cart_id
+import requests
 from django.http import HttpResponse
 # smtp imports
 from django.contrib.sites.shortcuts import get_current_site
@@ -61,9 +63,56 @@ def login(request):
 
         user = auth.authenticate(email=email,password=password)
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id =_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+                    # Getting the product variations by cart id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+                    
+                    # Get the cart items from the user to access his product variations
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variations = item.variations.all()
+                        ex_var_list.append(list(existing_variations))
+                        id.append(item.id)
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+            except:
+                pass
             auth.login(request,user)
             messages.success(request, "You are logged in now!")
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            print (url)
+            try:
+                query = requests.utils.urlparse(url).query
+                print(query)
+                params = dict(x.split('=') for x in query.split('&'))
+                print(params)
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request,"Invalid creditianals")
             return redirect('login')
