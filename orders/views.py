37c01ac94d2331984c6d5cb9b83e9c9ab6874d49
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-01-31 20:14:07 +0530

    implemented place order functionality

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from carts.models import CartItem 
+from .forms import OrderForm
+from .models import Order
+import datetime
+
+from django.http import HttpResponse
 
 # Create your views here.
+
+def place_order(request, total=0, quantity=0):
+    current_user = request.user
+
+    # If the cart count is less than or equal to 0, then redirect back to store
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('store')
+    
+    tax = 0
+    grand_total = 0
+
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
+    tax = (2 * total)/100
+    grand_total = total + tax
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            # store all the billing information in order table
+            data = Order()
+            print("storing")
+            data.user           = current_user
+            data.first_name     = form.cleaned_data['first_name']
+            data.last_name      = form.cleaned_data['last_name']
+            data.phone          = form.cleaned_data['phone']
+            data.email          = form.cleaned_data['email']
+            data.address_line_1 = form.cleaned_data['address_line_1']
+            data.address_line_2 = form.cleaned_data['address_line_2']
+            data.country        = form.cleaned_data['country']
+            data.state          = form.cleaned_data['state']
+            data.city           = form.cleaned_data['city']
+            data.order_note     = form.cleaned_data['order_note']
+            data.order_total    = grand_total
+            data.tax            = tax
+            data.ip             = request.META.get('REMOTE_ADDR')
+            data.save()
+            print('stored1')
+            # Genrate order number
+            current_date = datetime.date.today().strftime("%Y%m%d")
+            order_number = current_date + str(data.id)
+            data.order_number = order_number
+            data.save()
+            print('stored2')
+        return redirect('checkout')
+    else:
+        return redirect('checkout')
