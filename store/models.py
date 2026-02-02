fa83e53c246ee60d31fe48644895eccd360a23f1
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-02 21:10:44 +0530

    added ReviewRating model to store product reviews and ratings, linking to Product and Account models

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from category.models import category
 from django.urls import reverse
+from accounts.models import Account
 # Create your models here.
 
 class Product(models.Model):
@@ -34,13 +35,27 @@ Variation_category_choice = (
 )
 
 class Variation(models.Model):
-    product = models.ForeignKey(Product,max_length=100, on_delete=models.CASCADE)
-    variation_category = models.CharField(max_length=100, choices=Variation_category_choice)
-    variation_value = models.CharField(max_length=100)
-    is_active = models.BooleanField(default=True)
-    created_date = models.DateTimeField(auto_now_add=True)
+    product             = models.ForeignKey(Product,max_length=100, on_delete=models.CASCADE)
+    variation_category  = models.CharField(max_length=100, choices=Variation_category_choice)
+    variation_value     = models.CharField(max_length=100)
+    is_active           = models.BooleanField(default=True)
+    created_date        = models.DateTimeField(auto_now_add=True)
 
     objects = VariationManager()
     
     def __str__(self):
         return self.variation_value
+    
+class ReviewRating(models.Model):
+    product     = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user        = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject     = models.CharField(max_length=100, blank=True)
+    review      = models.TextField(max_length=500, blank=True)
+    rating      = models.FloatField()
+    ip          = models.CharField(max_length=20, blank=True)
+    status      = models.BooleanField(default=True)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
