fb4309669de4678c755ba012dc34350413e3dc84
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-04 20:13:30 +0530

    added UserProfileAdmin to manage user profiles in the admin interface, including a thumbnail display for profile pictures; registered UserProfile model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Account
+from .models import Account, UserProfile
 from django.contrib.auth.admin import UserAdmin
+from django.utils.html import format_html
 
 
 class AccountAdmin(UserAdmin):
@@ -13,6 +14,19 @@ class AccountAdmin(UserAdmin):
     list_filter = ()
     fieldsets = ()
 
+class UserProfileAdmin(admin.ModelAdmin):
+    def thumbnail(self, obj):
+        if obj.profile_picture:
+            return format_html(
+                '<img src="{}" width="30" style="border-radius:50%;">',
+                obj.profile_picture.url,
+            )
+        return "-"
+    thumbnail.short_description = "Profile Picture"
+
+    list_display = ("thumbnail", "user", "city", "state", "country")
+
 
 # Register your models here.
 admin.site.register(Account, AccountAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
