6bcd834455f2b17392213d4e66ac8000fc1c3959
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-05 16:36:22 +0530

    replaced admin URL with a fake_admin view that returns a forbidden response, enhancing security by restricting access to the admin interface

--- stylokart/urls.py
+++ stylokart/urls.py
@@ -19,9 +19,14 @@ from django.urls import path , include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.http import HttpResponseForbidden
+
+def fake_admin(request):
+    return HttpResponseForbidden("Access Denied")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/',fake_admin), 
+    path('secureloginsite/', admin.site.urls),
     path('',views.home ,name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
