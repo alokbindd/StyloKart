91cf564032e738ed3e370edb772667c96bcfb6e3
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-05 13:58:19 +0530

    added order_detail and change_password views to accounts/views.py, enhancing user account management with detailed order information and password update functionality; integrated login_required decorator for secure access

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegistrationForm, UserForm, UserProfileForm
 from .models import Account, UserProfile
-from orders.models import Order
+from orders.models import Order, OrderProduct
 from django.contrib import messages ,auth
 from django.contrib.auth.decorators import login_required
 from carts.models import Cart, CartItem
@@ -214,6 +214,7 @@ def resetPassword(request):
     else:
         return render(request,'accounts/resetPassword.html')
 
+@login_required(login_url='login')
 def my_orders(request):
     orders = Order.objects.filter(user_id=request.user.id, is_ordered=True).order_by('-created_at')
     context = {
@@ -222,6 +223,24 @@ def my_orders(request):
 
     return render(request,'accounts/my_orders.html',context)
 
+@login_required(login_url='login')
+def order_detail(request,order_id):
+    order_detail = OrderProduct.objects.filter(order__order_number=order_id)
+    order = Order.objects.get(order_number=order_id)
+    
+    subtotal = 0
+    for i in order_detail:
+        subtotal += i.product_price * i.quantity
+
+    context ={
+        'order_detail': order_detail,
+        'order': order,
+        'subtotal':subtotal,
+
+    }
+    return render(request,'accounts/order_detail.html',context)
+    
+@login_required(login_url='login')
 def edit_profile(request):
     userprofile = get_object_or_404(UserProfile, user_id=request.user.id)
     if request.method == 'POST':
@@ -242,3 +261,28 @@ def edit_profile(request):
         'userprofile':userprofile
     }
     return render(request,'accounts/edit_profile.html',context)
+
+@login_required(login_url='login')
+def change_password(request):
+    if request.method == 'POST':
+        current_password = request.POST['current_password']
+        new_password = request.POST['new_password']
+        confirm_password = request.POST['confirm_password']
+
+        user = Account.objects.get(username__exact=request.user.username)
+        if confirm_password == new_password:
+            success = user.check_password(current_password)
+            if success:
+                user.set_password(new_password)
+                user.save()
+                messages.success(request,"Password Updated successfully")
+                return redirect('change_password')
+            else:
+                messages.error(request,"Current password is incorrect")
+                return redirect('change_password')
+        else:
+            messages.error(request,"Password does not match!")
+            return redirect('change_password')
+    
+
+    return render(request,'accounts/change_password.html')
