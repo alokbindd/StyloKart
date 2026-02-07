dbb338f57318130cdbc28bb49e98842726d900bf
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-08 01:06:45 +0530

    updated secret key

--- stylokart/settings.py
+++ stylokart/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True,cast=bool)
+SECRET_KEY = config('SECRET_KEY', default='unsafe-dev-key',cast=bool)
 
 ALLOWED_HOSTS = ['django-stylokart-env.eba-egq2k9tf.us-west-2.elasticbeanstalk.com']
 
