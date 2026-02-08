35f1c6d7a0de3ea39d728c8c67d7c77beeb60869
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-08 21:19:42 +0530

    done configurations for s3 buckets

--- stylokart/settings.py
+++ stylokart/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'carts',
     'orders',
     'admin_honeypot',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -158,10 +159,27 @@ STATICFILES_DIRS = [
     'stylokart/static',
 ]
 
-# media files configuration
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+if DJANGO_ENV == 'production':
+    # =====================
+    # S3 MEDIA CONFIG
+    # =====================
 
+    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = 'us-west-2'
+
+    AWS_QUERYSTRING_AUTH = False
+    AWS_DEFAULT_ACL = 'public-read'
+
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
+else:
+    # media files configuration
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
 
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
