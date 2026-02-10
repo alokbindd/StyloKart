1b4917515524e0ffffce052e8532fa3c2049789b
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-10 12:01:23 +0530

    Fix cart context processor queryset bug

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -1,23 +1,27 @@
 from .models import Cart, CartItem
 from .views import _cart_id
 
-
 def counter(request):
     cart_count = 0
-    if 'admin' in request.path:
+
+    # Skip admin
+    if request.path.startswith('/admin') or request.path.startswith('/secureloginsite'):
         return {}
-    else:
-        try:
-            cart = Cart.objects.filter(cart_id = _cart_id(request))
-            if request.user.is_authenticated:
-                cart_items = CartItem.objects.all().filter(user=request.user)
+
+    try:
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user)
+        else:
+            cart = Cart.objects.filter(cart_id=_cart_id(request)).first()
+            if cart:
+                cart_items = CartItem.objects.filter(cart=cart)
             else:
-                cart_items = CartItem.objects.all().filter(cart=cart[:1])
-            
-            for cart_item in cart_items:
-                cart_count += cart_item.quantity
-        
-        except Cart.DoesNotExist:
-            cart_count = 0
-    
-    return dict(cart_count=cart_count)
+                cart_items = []
+
+        for item in cart_items:
+            cart_count += item.quantity
+
+    except Exception:
+        cart_count = 0
+
+    return {'cart_count': cart_count}
