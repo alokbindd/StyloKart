55e856b7a446cb705fa3d6a9c38a4cca92b5c722
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-12 00:01:15 +0530

    Migrate from SQLite to PostgreSQL and configure DATABASES via environment variables

--- stylokart/settings.py
+++ stylokart/settings.py
@@ -84,10 +84,16 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
