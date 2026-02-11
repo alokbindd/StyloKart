71267915cf4b8a2eb2d40801fc3964d8a7871a8a
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-12 00:53:20 +0530

    Fix ALLOWED_HOSTS for EB internal health checks

--- stylokart/settings.py
+++ stylokart/settings.py
@@ -27,8 +27,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY') or config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True,cast=bool)
 
-ALLOWED_HOSTS = ['stylokart-env.eba-pydmw3zp.ap-south-1.elasticbeanstalk.com']
-
+ALLOWED_HOSTS = [
+    "stylokart-env.eba-pydmw3zp.ap-south-1.elasticbeanstalk.com",
+    ".elasticbeanstalk.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 # Application definition
 
