6a2cf3d3368db46886afc9671f0bf5f5c004e6e7
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-12 20:28:00 +0530

    removed unused file

--- stylokart/debug_env.py
+++
@@ -1,8 +0,0 @@
-# views.py
-from django.http import HttpResponse
-from django.conf import settings
-
-def debug_env(request):
-    return HttpResponse(
-        f"DEBUG={settings.DEBUG}<br>DB={settings.DATABASES['default']['ENGINE']}"
-    )
