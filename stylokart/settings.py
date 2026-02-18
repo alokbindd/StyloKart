499b484775f417302ffeea9016119d29724f84d8
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-18 11:50:26 +0530

    added default config for email

--- stylokart/settings.py
+++ stylokart/settings.py
@@ -182,8 +182,8 @@ MESSAGE_TAGS = {
 
 # smtp configuration
 
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT',cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_PORT = config('EMAIL_PORT',cast=int, default=587)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
